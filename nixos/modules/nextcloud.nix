7c687033d910a08b97e8f4fed1ff8d3650d1d83c
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-10-03 18:48:22 +0300

    Quick fix nextcloud.nix

--- nixos/modules/nextcloud.nix
+++ nixos/modules/nextcloud.nix
@@ -13,11 +13,11 @@
     autoUpdateApps.enable = true;
     maxUploadSize = "1G";
 
-    settings.trusted_domains = [ 127.0.0.1/8 192.168.0.0/16 fd00::/8 ];
+    settings.trusted_domains = [ "127.0.0.1/8" "192.168.0.0/16" "fd00::/8" ];
 
     config = {
       dbtype = "pgsql";
       adminpassFile = "/etc/nextcloud-admin-pass";
-    }
+    };
   };
 }
