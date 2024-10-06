cef5d4953d8d2732aa4ad331687c843614f08f9b
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-10-06 14:45:14 +0300

    Quick fix firewall.nix

--- nixos/modules/firewall.nix
+++ nixos/modules/firewall.nix
@@ -2,6 +2,6 @@
   networking.firewall = {
     enable = true;                                # Enable the firewall
     allowedTCPPorts = [ 80 443 3478 3479 7454 ];  # Allow HTTP, HTTPS, CoTURN, and SSH on port 7454
-    allowedUDPPorts = [ 3478 3479 ];              # Allow CoTURN
+    allowedUDPPorts = [ 80 443 3478 3479 ];       # Allow CoTURN
   };
 }
