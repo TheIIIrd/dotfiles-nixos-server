a18a6fb4a77ce5ad5fdc670d1c9b5416beb011e7
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-10-06 13:40:00 +0300

    Quick fix coturn.nix

--- nixos/modules/coturn.nix
+++ nixos/modules/coturn.nix
@@ -2,6 +2,8 @@
   services.coturn = {
     enable = true;
     listening-port = 3478;
+    use-auth-secret = true;
+    static-auth-secret = "SECURE_SECRET_HERE";
     cert = "/var/lib/acme/nixos.nextcloud.local/fullchain.pem";
     pkey = "/var/lib/acme/nixos.nextcloud.local/key.pem";
   };
