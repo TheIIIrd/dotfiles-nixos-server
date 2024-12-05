b35b8d82059a79f135caa0ee87f288dbceec40c7
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-12-05 19:08:39 +0300

    Updated nixpkgs urls

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -36,5 +36,5 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
 
-  system.stateVersion = "24.05"; # Don't change it bro
+  system.stateVersion = "24.11"; # Don't change it bro
 }
